45817eb27bc332a2a475eed606a070147549226e
Shubham Bhosale <79128098+bshubham13@users.noreply.github.com>
2024-03-22 22:58:19 +0530

    StarPattern3.java

---
+++ StarPattern3.java
@@ -0,0 +1,26 @@
+/* 
+print the given pattern. 
+
+ *  *  *  *
+ 0  0  0  0
+ *  *  *  *
+ 0  0  0  0
+
+*/
+
+class StarPattern3 {
+	public static void main(String[] args) {
+
+		for (int i = 1; i <= 4; i++) {
+			for (int j = 1; j <= 4; j++) {
+				if (i % 2 == 0) {
+					System.out.print(" 0 ");
+				} else {
+					System.out.print(" * ");
+				}
+			}
+			System.out.println();
+		}
+
+	}
+}
